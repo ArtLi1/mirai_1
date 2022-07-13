b8ca876f658d1e22a9db265499cd5a54b469d2d4
ArtLRe <3385609893@qq.com>
2022-07-14 00:25:51 +0800

    7/14 v1

---
+++ src/main/java/org/example/Plugin.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+
+public final class Plugin extends JavaPlugin {
+    public static final Plugin INSTANCE = new Plugin();
+
+    private Plugin() {
+        super(new JvmPluginDescriptionBuilder("org.example.plugin", "1.0-SNAPSHOT").build());
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin loaded!");
+    }
+}
